05e98098c67213829d190260b78073a0ca51c1a3
interluder <18868112264@163.com>
2017-05-15 21:49:13 +0800

    Update VGA_CONTROLLER.v
    Modified timing parameters. Caused a lapse of 1 extra clock.
    Added a control signal (en_color) that can enable the RGB cable, which is asserted during the "display time".

--- scrs/VGA_CONTROLLER.v
+++ scrs/VGA_CONTROLLER.v
@@ -5,27 +5,31 @@
 
 module VGA_CONTROLLER
 # (
-	parameter				// V for vertical, H for horizontal
-		T_PW_V   = 10'd2,	// pulse width
-		T_BP_V   = 10'd31,	// back porch
-		T_DISP_V = 10'd511,	// display time
-		T_FP_V   = 10'd521,	// front porch
-		T_PW_H   = 10'd96,
-		T_BP_H   = 10'd144,
-		T_DISP_H = 10'd784,
-		T_FP_H   = 10'd800,
+	parameter
+		// V for vertical, H for horizontal
+		T_PW_V   = 10'd1,	// pulse width
+		T_BP_V   = 10'd30,	// back porch
+		T_DISP_V = 10'd510,	// display time
+		T_FP_V   = 10'd520,	// front porch
+		T_PW_H   = 10'd95,
+		T_BP_H   = 10'd143,
+		T_DISP_H = 10'd783,
+		T_FP_H   = 10'd799,
+
 		WIDTH    = 4'd10	// width of counters/positions
 )
 (
 	input rst_n,
 	input pixel_clk,
 	output reg [WIDTH-1:0] xpos, ypos,
+	output reg en_color,
 	output reg hsync, vsync
 );
 	wire Assert_hsync, Deassert_hsync;
 	wire Assert_vsync, Deassert_vsync;
 	wire Add_hcounter, Add_vcounter;
 	wire Enable_vertical_counter;
+	wire Assert_en_color;
 
 	reg [WIDTH-1:0] next_xpos, next_ypos;
 	reg [WIDTH-1:0] next_hcounter, next_vcounter;
@@ -43,6 +47,10 @@ module VGA_CONTROLLER
 	assign Add_hcounter = (hcounter <= T_FP_H-1);
 	assign Add_vcounter = (vcounter <= T_FP_V-1);
 	assign Enable_vertical_counter = Deassert_hsync;
+
+	// logic for enable_color signal
+	assign Assert_en_color = (    (next_hcounter>T_BP_H && next_hcounter<=T_DISP_H)
+				   && (next_vcounter>T_BP_V && next_vcounter<=T_DISP_V) );
 
 	// next hcounter logic
 	always @(*) begin
@@ -147,6 +155,20 @@ module VGA_CONTROLLER
 		else begin
 			xpos <= next_xpos;
 			ypos <= next_ypos;
+		end
+	end // always
+
+	// enable_color signal
+	always @(posedge pixel_clk or negedge rst_n) begin
+		if (!rst_n) begin
+			// reset
+			en_color <= 1'b0;
+		end
+		else if (Assert_en_color) begin
+			en_color <= 1'b1;
+		end
+		else begin
+			en_color <= 1'b0;
 		end
 	end // always
 
